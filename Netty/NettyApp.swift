310ce3f7c413c5cfa6f06b1a29b13f9e75f59af4
Danylo Siefierov <stuffeddanny@gmail.com>
2022-07-16 11:44:24 +0100

    Initial Commit

---
+++ Netty/NettyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NettyApp.swift
+//  Netty
+//
+//  Created by Danny on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NettyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
